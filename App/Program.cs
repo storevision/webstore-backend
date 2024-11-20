c444105911d0e65634b491aeb8b98d8bb5a094e3
Andreas Fercher <andreas.fercher.2603@gmail.com>
2024-11-20 14:20:47 +0100

    Add logic to connect with DB

--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,19 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using Webshop.Models.DB;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Lade die Umgebungsvariablen aus der .env-Datei
+Env.Load("backend.env"); 
+// Konfiguriere die Datenbankverbindung
+var connectionString = $"Host={Environment.GetEnvironmentVariable("POSTGRES_HOST")};" +
+                       $"Port={Environment.GetEnvironmentVariable("POSTGRES_PORT")};" + 
+                       $"Database={Environment.GetEnvironmentVariable("POSTGRES_DB")};" + 
+                       $"Username={Environment.GetEnvironmentVariable("POSTGRES_USER")};" + 
+                       $"Password={Environment.GetEnvironmentVariable("POSTGRES_PASSWORD")}";
+
+//Console.WriteLine($"ConnectionString: {connectionString}");
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -6,8 +21,15 @@ builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer(); // Für Endpunkte erforderlich
 builder.Services.AddSwaggerGen(); // Swagger-Dienst hinzufügen
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>(); 
+    ApplicationDbContext.TestDatabaseConnection(context);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ Models/DB/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Webshop.Models.DB;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    
+    public static void TestDatabaseConnection(ApplicationDbContext context)
+    {
+        try
+        {
+            var version = context.Database.ExecuteSqlRaw("SELECT version()");
+            Console.WriteLine("Database connection successful. PostgreSQL version: " + version);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Database connection failed: " + ex.Message);
+        }
+    }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
+
+public class Order
+{
+    public int Id { get; set; }
+    public string CustomerName { get; set; }
+    public List<Product> Products { get; set; }
+}
+
+
+
+
