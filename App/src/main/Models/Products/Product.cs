57504b5b34993cfd2b24b0ee92ec374b498767fc
Andreas Fercher <andreas.fercher.2603@gmail.com>
2024-11-26 12:52:27 +0100

    add controller for product management and logic to insert a new product and getById and get all from products

---
+++ App/src/main/Models/Products/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Webshop.Models.Products;
+
+public class Product
+{
+    public int productid { get; set; }
+    public string productname { get; set; }
+    public string productdescription { get; set; }
+    public string productprice { get; set; }
+}
