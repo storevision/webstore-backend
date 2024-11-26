a6664c8209b42ae9eeef5361261357f702f2823a
Andreas Fercher <andreas.fercher.2603@gmail.com>
2024-11-26 13:09:44 +0100

    repair

---
+++ App/src/main/Models/Cart/Order.cs
@@ -0,0 +1,24 @@
+﻿using Webshop.Models.Products;
+
+namespace Webshop.Models.Cart;
+
+public class Order
+{
+    public string orderId { get; set; }
+    public float sum { get; set; }
+    public DateTime date { get; set; }
+    Dictionary<int, Dictionary<int, object>> articleList { get; set; }
+    
+    public void AddArticleToList(Product product, int quantity) {
+        
+        Dictionary<int , object> article = new Dictionary<int , object>();
+        article.Add(quantity, product);
+        
+        articleList.Add(product.productid, article);
+        
+    }
+
+}
+
+
+
---
+++ App/src/main/Services/CartService.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using Webshop.Models.Cart;
+using Webshop.Models.Products;
+
+
+namespace Webshop.Services;
+
+public class CartService
+{
+    //Add an Article to the card
+    public static void addArticleToCart(int quantity, String productId)
+    
+    {
+        Product product = new Product(); // TODO waiting for the CRUD management from @David
+        
+        Order order = new Order();
+        order.AddArticleToList(product, quantity);
+
+    }
+
+    public static Product getProduct(String productId)
+    {
+        Product product = new Product();
+        return product;
+    }
+}
