f11476de08af3f1c0de5e07ff742e5cec2f236f2
DavidJ <david.janesch@gmail.com>
2024-12-10 19:33:26 +0100

    Implemented annotations to specify categories table.

--- App/src/main/Models/Category.cs
+++ App/src/main/Models/Category.cs
@@ -1,21 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 
 namespace Webshop.App.src.main.Models;
 
+[Table("categories")]
 public class Category
 {
     [Key]
     [Column("id")]
-    [JsonPropertyName("id")]
     public int CategoryID { get; set; }
+    [Required]
     [Column("name")]
-    [JsonPropertyName("name")]
     public string Name { get; set; }
-    [Column("description")]
-    [JsonPropertyName("description")]
-    [JsonIgnore]
-    public string Description { get; set; }
 }
