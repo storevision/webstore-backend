1d7437819f6611f19c91e1b6e5f481f5bc89718c
DavidJ <david.janesch@gmail.com>
2024-12-10 21:06:57 +0100

    Added inventory to data models

---
+++ App/src/main/Models/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Webshop.Models.Products;
+
+namespace Webshop.App.src.main.Models;
+
+[Table("inventory")]
+public class Inventory
+{
+    [Key]
+    [Column("id")]
+    public int InventoryId { get; set; }
+
+    [Required]
+    [Column("quantity")]
+    public int Quantity { get; set; }
+    
+    [ForeignKey("ProductId")]
+    public Product Product { get; set; }
+}
