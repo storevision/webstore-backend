d9aecde73ada5c613e7adf4013d8c6a126b31d00
Andreas Fercher <andreas.fercher.2603@gmail.com>
2024-12-10 09:29:02 +0100

    Modify product controller and product model, migration for the changed model

--- App/src/main/Models/Product.cs
+++ App/src/main/Models/Product.cs
@@ -8,8 +8,11 @@ namespace Webshop.Models.Products;
 public class Product
 {
     [Key]
+    //[Column("your_column_name")]
     public int ProductId { get; set; }
     public string ProductName { get; set; }
     public string? ProductDescription { get; set; }
     public decimal? ProductPrice { get; set; }
+    
+    public string? ProductImage { get; set; }
 }
