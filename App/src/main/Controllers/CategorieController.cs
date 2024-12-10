f764743a48554f3fe99312d522269680dc70b8aa
Andreas Fercher <andreas.fercher.2603@gmail.com>
2024-12-10 09:18:32 +0100

    Add category controller and category service, logic to get a list of categories

---
+++ App/src/main/Controllers/CategorieController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Webshop.App.src.main.Models;
+using Webshop.Services;
+
+namespace Webshop.Controllers;
+
+[ApiController]
+[Route("categories")]
+public class CategorieController : ControllerBase
+{
+    
+    private readonly CategorieService _categorieService;
+
+    public CategorieController(CategorieService categorieService)
+    {
+        _categorieService = categorieService;
+    }
+    
+    [HttpGet]
+    [Route("list")]
+    public async Task<IActionResult> GetCategories()
+    {
+        List<Category> categories = await _categorieService.GetAllCategoriesAsync();
+        return Ok(categories);
+    }
+}
