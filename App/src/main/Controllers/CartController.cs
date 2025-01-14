24d71e0321bf0e534e32b10ecbb2966e33987605
Andreas Fercher <andreas.fercher.2603@gmail.com>
2025-01-14 08:26:56 +0100

    include CartController and OrderController

---
+++ App/src/main/Controllers/CartController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webshop.App.src.main.Controllers
+{
+    [ApiController]
+    [Route("cart")]
+    public class CartController : ControllerBase
+    {
+        [HttpPost("add")]
+        public IActionResult AddToCart()
+        {
+            // Logik wird später hinzugefügt
+            return Ok("Add to cart endpoint hit.");
+        }
+
+        [HttpPost("remove")]
+        public IActionResult RemoveFromCart()
+        {
+            // Logik wird später hinzugefügt
+            return Ok("Remove from cart endpoint hit.");
+        }
+
+        [HttpGet("list")]
+        public IActionResult ListCartItems()
+        {
+            // Logik wird später hinzugefügt
+            return Ok("List cart items endpoint hit.");
+        }
+
+        [HttpPost("checkout")]
+        public IActionResult CheckoutCart()
+        {
+            // Logik wird später hinzugefügt
+            return Ok("Checkout cart endpoint hit.");
+        }
+
+        [HttpPost("clear")]
+        public IActionResult ClearCart()
+        {
+            // Logik wird später hinzugefügt
+            return Ok("Clear cart endpoint hit.");
+        }
+    }
+}
---
+++ App/src/main/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webshop.App.src.main.Controllers
+{
+    [ApiController]
+    [Route("orders")]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet("list")]
+        public IActionResult ListOrders()
+        {
+            // Logik wird später hinzugefügt
+            return Ok("List orders endpoint hit.");
+        }
+        // need a id to get the order
+        [HttpGet("get/{id}")]
+        public IActionResult GetOrder()
+        {
+            // Logik wird später hinzugefügt
+            return Ok("Get order endpoint hit.");
+        }
+    }
+}
